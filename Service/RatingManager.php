f4abc939579e1156b1a0939cc6505900c949060c
Bat <bpfefferkorn@linksip.fr>
2012-01-24 17:27:40 +0100

    fix typo

--- Service/RatingManager.php
+++ Service/RatingManager.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace DoctrineExtensions\Rateable;
+namespace SG\RateBundle\Service;
 
 use Doctrine\ORM\EntityManager;
 use DoctrineExtensions\Rateable\RatingManager as BaseRatingManager;
