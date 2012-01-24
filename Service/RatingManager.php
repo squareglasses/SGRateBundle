b7c2dc0beffa70e130fd4132d113ccf67a239cbf
Bat <bpfefferkorn@linksip.fr>
2012-01-24 11:07:29 +0100

    add service

---
+++ Service/RatingManager.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Rateable package.
+ * (c) Square Glasses <http://www.squareglasses.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DoctrineExtensions\Rateable;
+
+use Doctrine\ORM\EntityManager;
+use DoctrineExtensions\Rateable\RatingManager as BaseRatingManager;
+
+/**
+ * RatingManager.
+ *
+ * @author bat <contact@graindeweb.fr>
+ */
+class RatingManager extends BaseRatingManager
+{
+  
+}
