f98b42d653ad8bcb19d5080c67da707e8074e900
Bat <bpfefferkorn@linksip.fr>
2012-01-24 09:35:32 +0100

    fix config file type

--- DependencyInjection/SGRateExtension.php
+++ DependencyInjection/SGRateExtension.php
@@ -23,6 +23,6 @@ class SGRateExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.xml');
     }
 }
